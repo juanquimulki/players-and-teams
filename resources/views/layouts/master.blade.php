44c985696f3b420e806983a31227410e445fa527
Juan Mulki <ing.mulkiaguilera@gmail.com>
2023-07-18 10:00:36 -0300

    Add master layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Players & Teams - @yield('title')</title>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
+<script src="{{ mix('/js/app.js') }}"></script>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('title', 'Main Page')
+ 
+@section('content')
+    <div id="app">
+        <h1>Hello World!</h1>
+        
+        <b-button>Button</b-button>
+        <example-component></example-component>
+    </div>
+@stop
